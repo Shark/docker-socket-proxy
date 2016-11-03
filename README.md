b4566651fd58cd3636694cc71a1bc56b49645d82
Felix Seidel <felix@seidel.me>
2016-11-03 21:20:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# docker-socket-proxy
+
+## Building
+
+- Clone the repository in your `$GOPATH/src/github.com/Shark/docker-socket-proxy`
+- Run `go build .`
+
+## Usage
+
+Run `./docker-socket-proxy`
+
+## Contributing
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request! :)
+
+## History
+
+- v0.0.1 (2016-11-03): initial version
+
+## License
+
+This project is licensed under the MIT License. See LICENSE for details.
---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func handleErr(writer http.ResponseWriter, status int, err error) {
+	msg := fmt.Sprintf("%v", err)
+	log.Print(msg)
+
+	msgJson := struct {
+		Message string `json:"message"`
+	}{msg}
+	bytes, err := json.Marshal(msgJson)
+	if err != nil {
+		log.Printf("Error encoding json: %v", err)
+		return
+	}
+
+	writer.WriteHeader(status)
+	writer.Write(bytes)
+}
+
+type handler struct{}
+
+func (h *handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		handleErr(writer, 403, fmt.Errorf("Unauthorized request to %s %s", request.Method, request.URL.Path))
+		return
+	}
+
+	conn, err := net.DialUnix("unix", nil, &net.UnixAddr{"/var/run/docker.sock", "unix"})
+	if err != nil {
+		handleErr(writer, 500, err)
+		return
+	}
+	defer conn.Close()
+	err = request.Write(conn)
+	if err != nil {
+		handleErr(writer, 500, err)
+		return
+	}
+	resp, err := http.ReadResponse(bufio.NewReader(conn), request)
+	if err != nil {
+		handleErr(writer, 500, err)
+		return
+	}
+	defer resp.Body.Close()
+	for k, v := range resp.Header {
+		writer.Header()[k] = v
+	}
+	writer.WriteHeader(resp.StatusCode)
+	io.Copy(writer, resp.Body)
+}
+
+func main() {
+	sock, err := net.Listen("unix", "docker-socket-proxy.sock")
+	if err != nil {
+		log.Fatalf("Can not listen: %v", err)
+		return
+	}
+
+	myHandler := &handler{}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, os.Kill, syscall.SIGTERM)
+	go func(c chan os.Signal) {
+		sig := <-sigChan
+		log.Printf("Got signal %v: exiting", sig)
+		sock.Close()
+		os.Exit(0)
+	}(sigChan)
+
+	err = http.Serve(sock, myHandler)
+	if err != nil {
+		log.Fatalf("Failed to serve http: %v", err)
+		return
+	}
+}
